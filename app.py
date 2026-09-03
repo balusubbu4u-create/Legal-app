6c1a4a4d03fb36cf7c6b2492a718d0e5c27f5088
balusubbu4u-create <balu.subbu4u@gmail.com>
2026-09-03 14:52:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ if st.button("కేస్ విశ్లేషించండి (Analyze)", t
                     )
 
                 response = client.models.generate_content(
-                    model="gemini-1.5-flash-latest",
+                    model="gemini-1.5-flash-8b",
                     contents=content
                 )
 
